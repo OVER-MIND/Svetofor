d1db5bdc95c10b81e6e2c4b83eb4f25af02c7a12
Kostya Petrishchev <55559870+OVER-MIND@users.noreply.github.com>
2019-09-24 13:47:59 +0300

    Add files via upload

---
+++ sketch_sep24a.ino
@@ -0,0 +1,31 @@
+void setup()
+{
+  pinMode(10, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(1, OUTPUT);
+}
+
+  void loop ()
+{
+   digitalWrite (5, HIGH); 
+  delay (1000); 
+   digitalWrite (5, LOW);
+  delay (500);
+  
+   digitalWrite (10, HIGH); 
+  delay (2000); 
+   digitalWrite (10, LOW);
+  delay (1000);
+        
+    digitalWrite (5, HIGH); 
+  delay (1000); 
+   digitalWrite (5, LOW);
+  delay (500);
+  
+   digitalWrite (1, HIGH); 
+  delay (2000); 
+   digitalWrite (1, LOW);
+  delay (1000);
+     
+
+}
