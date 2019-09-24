a6c3bea8808cdc245c7215fc4f434755699dce67
Kostya Petrishchev <55559870+OVER-MIND@users.noreply.github.com>
2019-09-24 21:28:10 +0300

    Update sketch_sep24a.ino

--- sketch_sep24a.ino
+++ sketch_sep24a.ino
@@ -7,25 +7,25 @@ void setup()
 
   void loop ()
 {
-   digitalWrite (10, HIGH); 
-  delay (1000); 
-   digitalWrite (10, LOW);
-  delay (1000);
+        digitalWrite (10, HIGH); 
+        delay (1000); 
+        digitalWrite (10, LOW);
+        delay (1000);
   
-   digitalWrite (5, HIGH); 
-  delay (1000); 
-   digitalWrite (5, LOW);
-  delay (1000);
+              digitalWrite (5, HIGH); 
+              delay (1000); 
+              digitalWrite (5, LOW);
+               delay (1000);
         
-    digitalWrite (1, HIGH); 
-  delay (1000); 
-   digitalWrite (1, LOW);
-  delay (1000);
-  
-   digitalWrite (5, HIGH); 
-  delay (1000); 
-   digitalWrite (5, LOW);
-  delay (1000);
+                   digitalWrite (1, HIGH); 
+                   delay (1000); 
+                   digitalWrite (1, LOW);
+                   delay (1000);
+
+                       digitalWrite (5, HIGH); 
+                       delay (1000); 
+                       digitalWrite (5, LOW);
+                       delay (1000);
      
 
 }
