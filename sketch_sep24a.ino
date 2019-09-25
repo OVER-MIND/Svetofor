c974c5b36b863a26fa38749e305ff383c89d2aec
Kostya Petrishchev <55559870+OVER-MIND@users.noreply.github.com>
2019-09-25 22:23:00 +0300

    Na 35b. No chet poka ne robit((((

--- sketch_sep24a.ino
+++ sketch_sep24a.ino
@@ -1,36 +1,55 @@
+int knopka = 12;          //это кнопочка
 int L_Red = 10 ;          //это красненький
 int L_Yellow = 5 ;        //это желтенький
-int L_Green = 1 ;         //это зелененький
+int L_Green = 3 ;         //это зелененький
 
+void setup() {
+      Serial.begin (96000);
+      pinMode(L_Red, OUTPUT);
+      pinMode(L_Yellow, OUTPUT);
+      pinMode(L_Green, OUTPUT);
+      pinMode(knopka, INPUT);
 
-void setup()
-{
-  	pinMode(L_Red, OUTPUT);
- 	pinMode(L_Yellow, OUTPUT);
-  	pinMode(L_Green, OUTPUT);
 }
 
-  void loop ()
-{
-        digitalWrite (L_Red, HIGH); 
-        delay (1000); 
+void loop() {
+
+  if (digitalRead(knopka) == HIGH) {
+
+        digitalWrite (L_Yellow, HIGH);
+        delay (1000);
+        digitalWrite (L_Yellow, LOW);
+        delay (1000);
+        
+          digitalWrite (L_Red, HIGH);
+          delay (1000);
+          digitalWrite (L_Red, LOW);
+          delay (1000);
+
+  }
+
+  else {
+
+        digitalWrite (L_Red, HIGH);
+        delay (1000);
         digitalWrite (L_Red, LOW);
         delay (1000);
-  
-              digitalWrite (L_Yellow, HIGH); 
-              delay (1000); 
-              digitalWrite (L_Yellow, LOW);
-               delay (1000);
-        
-                   digitalWrite (L_Green, HIGH); 
-                   delay (1000); 
-                   digitalWrite (L_Green, LOW);
-                   delay (1000);
 
-                       digitalWrite (L_Yellow, HIGH); 
-                       delay (1000); 
-                       digitalWrite (L_Yellow, LOW);
-                       delay (1000);
-     
+          digitalWrite (L_Yellow, HIGH);
+          delay (1000);
+          digitalWrite (L_Yellow, LOW);
+          delay (1000);
+
+            digitalWrite (L_Green, HIGH);
+            delay (1000);
+            digitalWrite (L_Green, LOW);
+            delay (1000);
+
+              digitalWrite (L_Yellow, HIGH);
+              delay (1000);
+              digitalWrite (L_Yellow, LOW);
+              delay (1000);
+
+  }
 
 }
