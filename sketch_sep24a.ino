85eb658bb1a8bfadc36af39d83ae0bfbc2da8c14
Kostya Petrishchev <55559870+OVER-MIND@users.noreply.github.com>
2019-10-04 19:07:40 +0300

    Hy I pitalsya

--- sketch_sep24a.ino
+++ sketch_sep24a.ino
@@ -1,9 +1,9 @@
 boolean butt_flag = 0;
 boolean butt ;
-int knopka = 13;          //это кнопочка
-int L_Red = 10 ;          //это красненький
-int L_Yellow = 5 ;        //это желтенький
-int L_Green = 3 ;         //это зелененький
+#define knopka  13;          //это кнопочка
+#define L_Red 10 ;          //это красненький
+#define L_Yellow 5 ;        //это желтенький
+#define L_Green 3 ;         //это зелененький
 
 void setup() {
       pinMode(L_Red, OUTPUT);
